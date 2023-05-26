ab33a06b9405a940da588bf82fb914654891dc0b
24Victor <borenvictorzheng@iesmontsia.org>
2023-05-26 17:32:15 +0200

    fsa

--- Login i register/php/alumnat.php
+++ Login i register/php/alumnat.php
@@ -4,7 +4,7 @@ session_start();
 if (!isset($_SESSION['usuari'])) {
     echo '
         <script>
-            alert("Per favor iniciar sessio");
+            alert("Per favor iniciar sessió");
             window.location = "../index.php";
         </script>
     ';
@@ -12,6 +12,18 @@ if (!isset($_SESSION['usuari'])) {
     session_destroy();
     die();
 }
+
+// Establecer la conexión a la base de datos (suponiendo que ya tienes la conexión)
+include 'connexio.php';
+// Consulta para obtener el listado de portátiles por cada aula en el alumnat
+$queryPortatilsAlumnat = "SELECT aula, portatils FROM alumnat";
+$resultPortatilsAlumnat = mysqli_query($conexio, $queryPortatilsAlumnat);
+
+// Consulta para obtener el estado del disco en el professorat
+$queryEstatDiscsProfessorat = "SELECT aula, discs_incidencia FROM professorat";
+$resultEstatDiscsProfessorat = mysqli_query($conexio, $queryEstatDiscsProfessorat);
+
+// Recorrer los resultados y mostrar los listados
 ?>
 
 <!DOCTYPE html>
@@ -21,8 +33,91 @@ if (!isset($_SESSION['usuari'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: black;
+            color: white;
+            text-align: center;
+            padding: 20px;
+        }
+
+        h1, h2 {
+            text-align: center;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+
+        tr:hover {
+            background-color: #f5f5f5;
+        }
+
+        th {
+            background-color: #4CAF50;
+            color: white;
+        }
+
+        .logout-link {
+            display: block;
+            text-align: center;
+            margin-top: 20px;
+        }
+    </style>
 </head>
 <body>
-    <h1>Pàgina Alumnat</h1>
+    <header>
+        <h1>Pàgina Alumnat</h1>
+    </header>
+
+    <h2>Estat del seu ordinador:</h2>
+    <table>
+        <tr>
+            <th>Aula</th>
+            <th>Portàtils</th>
+        </tr>
+        <?php while ($row = mysqli_fetch_assoc($resultPortatilsAlumnat)): ?>
+        <tr>
+            <td><?php echo $row['aula']; ?></td>
+            <td><?php echo $row['portatils']; ?></td>
+        </tr>
+        <?php endwhile; ?>
+    </table>
+
+    <h2>Estat del seu disc:</h2>
+    <table>
+        <tr>
+            <th>Aula</th>
+            <th>Discs en incidència</th>
+        </tr>
+        <?php while ($row = mysqli_fetch_assoc($resultEstatDiscsProfessorat)): ?>
+        <tr>
+            <td><?php echo $row['aula']; ?></td>
+            <td><?php echo $row['discs_incidencia']; ?></td>
+        </tr>
+        <?php endwhile; ?>
+    </table>
+    
+    <a href="php/tanca_sessio.php" class="logout-link">Tanca Sessió</a>
+    <script>
+        document.querySelector("a[href='php/tanca_sessio.php']").addEventListener("click", function(e) {
+            e.preventDefault();
+            window.location.href = "../index.php";
+        });
+    </script>
 </body>
 </html>
--- Login i register/php/professorat.php
+++ Login i register/php/professorat.php
@@ -6,7 +6,20 @@ if (!isset($_SESSION['usuari'])) {
     session_destroy();
     die();
 }
+
+// Establecer la conexión a la base de datos (suponiendo que ya tienes la conexión)
+include 'connexio.php';
+// Consulta para obtener el listado de portátiles por cada aula
+$queryPortatils = "SELECT aula, portatils FROM professorat";
+$resultPortatils = mysqli_query($conexio, $queryPortatils);
+
+// Consulta para obtener la lista de discos duros en incidencia
+$queryDiscsIncidencia = "SELECT aula, discs_incidencia FROM professorat WHERE discs_incidencia > 0";
+$resultDiscsIncidencia = mysqli_query($conexio, $queryDiscsIncidencia);
+
+// Recorrer los resultados y mostrar los listados
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,8 +27,91 @@ if (!isset($_SESSION['usuari'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: black;
+            color: white;
+            text-align: center;
+            padding: 20px;
+        }
+
+        h1, h2 {
+            text-align: center;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+
+        tr:hover {
+            background-color: #f5f5f5;
+        }
+
+        th {
+            background-color: #4CAF50;
+            color: white;
+        }
+
+        .logout-link {
+            display: block;
+            text-align: center;
+            margin-top: 20px;
+        }
+    </style>
 </head>
 <body>
-    <h1>Pàgina Professorat</h1>
+    <header>
+        <h1>Pàgina Professorat</h1>
+    </header>
+
+    <h2>Llistat dels portàtils per aula:</h2>
+    <table>
+        <tr>
+            <th>Aula</th>
+            <th>Portàtils</th>
+        </tr>
+        <?php while ($row = mysqli_fetch_assoc($resultPortatils)): ?>
+        <tr>
+            <td><?php echo $row['aula']; ?></td>
+            <td><?php echo $row['portatils']; ?></td>
+        </tr>
+        <?php endwhile; ?>
+    </table>
+
+    <h2>Llista dels discs durs en incidència:</h2>
+    <table>
+        <tr>
+            <th>Aula</th>
+            <th>Discs en incidència</th>
+        </tr>
+        <?php while ($row = mysqli_fetch_assoc($resultDiscsIncidencia)): ?>
+        <tr>
+            <td><?php echo $row['aula']; ?></td>
+            <td><?php echo $row['discs_incidencia']; ?></td>
+        </tr>
+        <?php endwhile; ?>
+    </table>
+
+    <a href="php/tanca_sessio.php" class="logout-link">Tanca Sessió</a>
+    <script>
+        document.querySelector("a[href='php/tanca_sessio.php']").addEventListener("click", function(e) {
+            e.preventDefault();
+            window.location.href = "../index.php";
+        });
+    </script>
 </body>
 </html>
